c3c7677d137f844e809ddea31e4f1cc0d1204950
Vlad Chub <vlad.chub@gmail.com>
2012-10-10 15:19:19 +0400

    CampaingHistory, MegaModel oh....

--- app/domain/CampaignHistory.scala
+++ app/domain/CampaignHistory.scala
@@ -4,31 +4,27 @@ import scala.reflect._
 import org.joda.time._
 
 /** Main strucure for Optimizer
-* contains flatmap of the domain model which aggregations are mapped to CampaignHistory attribues
-* CampaignHistory has startDate and endDate
+* It contains partially flatten map of the Campaign which multiple level aggregations are mapped
+* and sometimes reodered to CampaignHistory's attribues
+* All Histories startDate and endDate are in complience w/ CampaignHistory.startDate and endDate
 * CampaignHistory generally is created by DAO and passed to Optimizer by Controller
 */
 
 @BeanInfo
 case class CampaignHistory(
+  val campaign: Campaign,
   val startDate: DateTime,
   val endDate: DateTime,
 
-  val campaign: Campaign,
-  val bannerPhrases: Seq[BannerPhrase],
-
-  val curveHistory: CurveHistory,
+  val curves: Seq[Curve],
   val campaignPerformanceHistory: PerformanceHistory,
+
+  // bannerPhrases realized aggregations include performanceHistory, actualBidHistory
+  // and recommendedBidHistory
+  val bannerPhrases: Seq[BannerPhrase],
 
   val permutationHistory: PermutationHistory,
   val optimalPermutationHistory: PermutationHistory,
-
-
-  //TODO: PerformanceHistory.startDate and endDate have to comply to 
-  // CampaignHistory.startDate and CampaignHistory.endDate
-  val bannerPhrasePerformanceHistory: Seq[(DateTime, Map[BannerPhrase, PerformanceHistory])],
-  val bannerPhraseActualBidHistory: Seq[(DateTime, Map[BannerPhrase, TimeSeries])],
-  val bannerPhraseRecommendedBidHistory: Seq[(DateTime, Map[BannerPhrase, TimeSeries])]
 ){}
 
 
