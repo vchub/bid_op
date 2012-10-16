7d0aba6ec160a88ef56e2042a80a9acb0a46681b
Vlad Chub <vlad.chub@gmail.com>
2012-10-16 11:52:41 +0400

    change diagramm, turn off evolutions in app.conf

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,45 +3,14 @@
 
 # --- !Ups
 
-
 -- table declarations :
-create table "Banner" (
-    "id" bigint primary key not null,
-    "network_banner_id" varchar(128) not null,
-    "campaign_id" bigint not null
+create table "ad_user" (
+    "name" varchar(128) not null,
+    "id" bigint primary key not null
   );
-create sequence "s_Banner_id";
-create table "BannerPhrase" (
-    "banner_id" bigint not null,
-    "region_id" bigint not null,
-    "id" bigint primary key not null,
-    "phrase_id" bigint not null,
-    "bid" double precision not null
-  );
-create sequence "s_BannerPhrase_id";
-create table "BannerPhraseStats" (
-    "impress_context" integer not null,
-    "start_date" date not null,
-    "impress_search" integer not null,
-    "clicks_context" integer not null,
-    "clicks_search" integer not null,
-    "sum_context" double precision not null,
-    "id" bigint primary key not null,
-    "end_date" date not null,
-    "sum_search" double precision not null,
-    "bannerphrase_id" bigint not null
-  );
-create sequence "s_BannerPhraseStats_id";
-create table "Curve" (
-    "start_date" timestamp not null,
-    "a" double precision not null,
-    "id" bigint primary key not null,
-    "b" double precision not null,
-    "c" double precision not null,
-    "campaign_id" bigint not null,
-    "d" double precision not null
-  );
-create sequence "s_Curve_id";
+create sequence "s_ad_user_id";
+-- indexes on ad_user
+create unique index "idx1eb904b2" on "ad_user" ("name");
 create table "Network" (
     "name" varchar(128) not null,
     "id" bigint primary key not null
@@ -49,26 +18,12 @@ create table "Network" (
 create sequence "s_Network_id";
 -- indexes on Network
 create unique index "idx1ed504b9" on "Network" ("name");
-create table "Permutation" (
-    "timeslot_id" bigint not null,
+create table "Banner" (
     "id" bigint primary key not null,
-    "position" integer not null,
-    "bid" double precision not null,
-    "bannerphrase_id" bigint not null
-  );
-create sequence "s_Permutation_id";
-create table "Phrase" (
-    "network_phrase_id" varchar(128) not null,
-    "phrase" varchar(128) not null,
-    "id" bigint primary key not null
-  );
-create sequence "s_Phrase_id";
-create table "Recommendation" (
-    "start_date" timestamp not null,
-    "id" bigint primary key not null,
+    "network_banner_id" varchar(128) not null,
     "campaign_id" bigint not null
   );
-create sequence "s_Recommendation_id";
+create sequence "s_Banner_id";
 create table "Region" (
     "parent_id" bigint not null,
     "description" varchar(128) not null,
@@ -77,40 +32,119 @@ create table "Region" (
     "network_region_id" varchar(128) not null
   );
 create sequence "s_Region_id";
-create table "TimeSlot" (
+create table "Phrase" (
+    "network_phrase_id" varchar(128) not null,
+    "phrase" varchar(128) not null,
+    "id" bigint primary key not null
+  );
+create sequence "s_Phrase_id";
+create table "BannerPhrase" (
+    "banner_id" bigint not null,
+    "region_id" bigint not null,
+    "id" bigint primary key not null,
+    "phrase_id" bigint not null
+  );
+create sequence "s_BannerPhrase_id";
+create table "Position" (
+    "permutation_id" bigint not null,
+    "id" bigint primary key not null,
+    "position" integer not null,
+    "bannerphrase_id" bigint not null
+  );
+create sequence "s_Position_id";
+create table "PeriodType" (
+    "description" varchar(128) not null,
+    "factor" double precision not null,
+    "id" bigint primary key not null
+  );
+create sequence "s_PeriodType_id";
+create table "Curve" (
+    "a" double precision not null,
+    "id" bigint primary key not null,
+    "b" double precision not null,
+    "date" timestamp not null,
+    "c" double precision not null,
+    "optimalPermutation_id" bigint not null,
+    "campaign_id" bigint not null,
+    "d" double precision not null
+  );
+create sequence "s_Curve_id";
+create table "CampaignPerformance" (
+    "cost_search" double precision not null,
     "impress_context" integer not null,
-    "start_date" timestamp not null,
-    "curve_id" bigint not null,
     "impress_search" integer not null,
     "clicks_context" integer not null,
     "clicks_search" integer not null,
-    "sum_context" double precision not null,
+    "cost_context" double precision not null,
     "id" bigint primary key not null,
-    "end_date" timestamp not null,
-    "timeslottype_id" bigint not null,
-    "sum_search" double precision not null
+    "date" date not null,
+    "periodtype_id" bigint not null,
+    "campaign_id" bigint not null
   );
-create sequence "s_TimeSlot_id";
-create table "TimeSlotType" (
-    "description" varchar(128) not null,
-    "id" bigint primary key not null
+create sequence "s_CampaignPerformance_id";
+create table "BannerPhrasePerformance" (
+    "cost_search" double precision not null,
+    "impress_context" integer not null,
+    "impress_search" integer not null,
+    "clicks_context" integer not null,
+    "clicks_search" integer not null,
+    "cost_context" double precision not null,
+    "id" bigint primary key not null,
+    "date" date not null,
+    "periodtype_id" bigint not null,
+    "bannerphrase_id" bigint not null
   );
-create sequence "s_TimeSlotType_id";
-create table "ad_user" (
-    "name" varchar(128) not null,
-    "id" bigint primary key not null
+create sequence "s_BannerPhrasePerformance_id";
+create table "Permutation" (
+    "curve_id" bigint not null,
+    "id" bigint primary key not null,
+    "date" date not null
   );
-create sequence "s_ad_user_id";
--- indexes on ad_user
-create unique index "idx1eb904b2" on "ad_user" ("name");
+create sequence "s_Permutation_id";
+create table "ActualBidHistory" (
+    "id" bigint primary key not null,
+    "date" date not null,
+    "bid" double precision not null,
+    "bannerphrase_id" bigint not null
+  );
+create sequence "s_ActualBidHistory_id";
+create table "RecommendationHistory" (
+    "id" bigint primary key not null,
+    "date" date not null,
+    "bid" double precision not null,
+    "bannerphrase_id" bigint not null
+  );
+create sequence "s_RecommendationHistory_id";
+create table "NetAdvisedBidHistory" (
+    "a" double precision not null,
+    "id" bigint primary key not null,
+    "b" double precision not null,
+    "date" date not null,
+    "c" double precision not null,
+    "bannerphrase_id" bigint not null,
+    "d" double precision not null
+  );
+create sequence "s_NetAdvisedBidHistory_id";
+create table "BudgetHistory" (
+    "id" bigint primary key not null,
+    "date" date not null,
+    "budget" double precision not null,
+    "campaign_id" bigint not null
+  );
+create sequence "s_BudgetHistory_id";
+create table "EndDateHistory" (
+    "endDate" date not null,
+    "id" bigint primary key not null,
+    "date" date not null,
+    "campaign_id" bigint not null
+  );
+create sequence "s_EndDateHistory_id";
 create table "Campaign" (
     "network_id" bigint not null,
-    "start_date" date not null,
     "user_id" bigint not null,
     "network_campaign_id" varchar(128) not null,
     "id" bigint primary key not null,
-    "end_date" date not null,
-    "budget" double precision not null
+    "startDate" date not null
   );
 create sequence "s_Campaign_id";
 -- indexes on Campaign
@@ -118,15 +152,21 @@ create unique index "idx9f2b0b23" on "Campaign" ("network_campaign_id");
 -- foreign key constraints :
 alter table "Banner" add constraint "BannerFK3" foreign key ("campaign_id") references "Campaign"("id");
 alter table "Curve" add constraint "CurveFK4" foreign key ("campaign_id") references "Campaign"("id");
-alter table "Recommendation" add constraint "RecommendationFK5" foreign key ("campaign_id") references "Campaign"("id");
-alter table "BannerPhrase" add constraint "BannerPhraseFK6" foreign key ("banner_id") references "Banner"("id");
-alter table "BannerPhrase" add constraint "BannerPhraseFK7" foreign key ("phrase_id") references "Phrase"("id");
-alter table "BannerPhrase" add constraint "BannerPhraseFK8" foreign key ("region_id") references "Region"("id");
-alter table "BannerPhraseStats" add constraint "BannerPhraseStatsFK9" foreign key ("bannerphrase_id") references "BannerPhrase"("id");
-alter table "Permutation" add constraint "PermutationFK10" foreign key ("bannerphrase_id") references "BannerPhrase"("id");
-alter table "TimeSlot" add constraint "TimeSlotFK11" foreign key ("curve_id") references "Curve"("id");
-alter table "TimeSlot" add constraint "TimeSlotFK12" foreign key ("timeslottype_id") references "TimeSlotType"("id");
-alter table "Permutation" add constraint "PermutationFK13" foreign key ("timeslot_id") references "TimeSlot"("id");
+alter table "CampaignPerformance" add constraint "CampaignPerformanceFK5" foreign key ("campaign_id") references "Campaign"("id");
+alter table "BudgetHistory" add constraint "BudgetHistoryFK6" foreign key ("campaign_id") references "Campaign"("id");
+alter table "EndDateHistory" add constraint "EndDateHistoryFK7" foreign key ("campaign_id") references "Campaign"("id");
+alter table "BannerPhrase" add constraint "BannerPhraseFK8" foreign key ("banner_id") references "Banner"("id");
+alter table "BannerPhrase" add constraint "BannerPhraseFK9" foreign key ("phrase_id") references "Phrase"("id");
+alter table "BannerPhrase" add constraint "BannerPhraseFK10" foreign key ("region_id") references "Region"("id");
+alter table "Position" add constraint "PositionFK11" foreign key ("bannerphrase_id") references "BannerPhrase"("id");
+alter table "BannerPhrasePerformance" add constraint "BannerPhrasePerformanceFK12" foreign key ("bannerphrase_id") references "BannerPhrase"("id");
+alter table "ActualBidHistory" add constraint "ActualBidHistoryFK13" foreign key ("bannerphrase_id") references "BannerPhrase"("id");
+alter table "RecommendationHistory" add constraint "RecommendationHistoryFK14" foreign key ("bannerphrase_id") references "BannerPhrase"("id");
+alter table "NetAdvisedBidHistory" add constraint "NetAdvisedBidHistoryFK15" foreign key ("bannerphrase_id") references "BannerPhrase"("id");
+alter table "Permutation" add constraint "PermutationFK16" foreign key ("curve_id") references "Curve"("id");
+alter table "Position" add constraint "PositionFK17" foreign key ("permutation_id") references "Permutation"("id");
+alter table "BannerPhrasePerformance" add constraint "BannerPhrasePerformanceFK18" foreign key ("periodtype_id") references "PeriodType"("id");
+alter table "CampaignPerformance" add constraint "CampaignPerformanceFK19" foreign key ("periodtype_id") references "PeriodType"("id");
 alter table "Campaign" add constraint "CampaignFK1" foreign key ("user_id") references "ad_user"("id");
 alter table "Campaign" add constraint "CampaignFK2" foreign key ("network_id") references "Network"("id");
 
@@ -134,7 +174,9 @@ alter table "Campaign" add constraint "CampaignFK2" foreign key ("network_id") r
 
 
 
+
 # --- !Downs
+--- TODO: Update 
 
 drop table if exists  Banner                 CASCADE;
 drop table if exists  BannerPhrase           CASCADE;
