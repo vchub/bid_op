0a1652abf9182abb5e8ba347b600c993713d6d0c
krispo <konstantin.skipor@gmail.com>
2013-02-19 11:02:33 +0400

    added tests for json_api with Campaign type

--- test/json_api/Convert_fromJsonSpec.scala
+++ test/json_api/Convert_fromJsonSpec.scala
@@ -5,9 +5,14 @@ import serializers._
 
 import org.specs2.mutable._
 import org.specs2.specification._
+
 import play.api.libs.json._
 
+import org.joda.time.format
+
 class Convert_fromJsonSpec extends Specification with AllExpectations {
+
+  val iso_fmt = format.ISODateTimeFormat.dateTime()
 
   /*------------- User ---------------------------------------------------*/
   "fromJson - User" should {
@@ -22,6 +27,59 @@ class Convert_fromJsonSpec extends Specification with AllExpectations {
 
       res.name must_== ("krisp0")
       res.password must_== ("123")
+    }
+  }
+
+  /*------------- Campaign ---------------------------------------------------*/
+  "fromJson - Campaign" should {
+    sequential
+
+    "take TRUE data" in {
+      val date = iso_fmt.parseDateTime("2013-01-01T12:00:00.000+04:00")
+
+      val data = """
+       {"start_date": %d,
+        "end_date": %d,
+        "_login": "krisp0",
+        "_token": "123",
+        "network_campaign_id": "100",        
+        "daily_budget": 50.0}""".format(date.getMillis(), date.plusDays(1).getMillis())
+
+      val Some(res) = fromJson[Campaign](Json.parse(data))
+
+      res._login must_== ("krisp0")
+      res._token must_== ("123")
+      res.start_date must_== (date)
+    }
+  }
+  /*------------- List[Campaign] ---------------------------------------------------*/
+  "fromJson - List[Campaign]" should {
+    sequential
+
+    "take TRUE data" in {
+      val date = iso_fmt.parseDateTime("2013-01-01T12:00:00.000+04:00")
+
+      val data = """[
+       {"start_date": %d,
+        "end_date": %d,
+        "_login": "krisp0",
+        "_token": "123",
+        "network_campaign_id": "100",        
+        "daily_budget": 50.0},
+       {"start_date": %d,
+        "end_date": %d,
+        "_login": "krisp0",
+        "_token": "123",
+        "network_campaign_id": "100",        
+        "daily_budget": 50.0}
+       ]""".format(date.getMillis(), date.plusDays(1).getMillis(), date.getMillis(), date.plusDays(1).getMillis())
+
+      val Some(res) = fromJson[List[Campaign]](Json.parse(data))
+
+      res.head._login must_== ("krisp0")
+      res.head._token must_== ("123")
+      res.head.start_date must_== (date)
+      res.last.daily_budget must_== (50.0)
     }
   }
 }
--- test/json_api/Convert_toJsonSpec.scala
+++ test/json_api/Convert_toJsonSpec.scala
@@ -5,9 +5,15 @@ import serializers._
 
 import org.specs2.mutable._
 import org.specs2.specification._
+
 import play.api.libs.json._
 
+import org.joda.time.format
+import org.joda.time._
+
 class Convert_toJsonSpec extends Specification with AllExpectations {
+
+  val iso_fmt = format.ISODateTimeFormat.dateTime()
 
   /*------------- User ---------------------------------------------------*/
   "toJson - User" should {
@@ -20,6 +26,41 @@ class Convert_toJsonSpec extends Specification with AllExpectations {
 
       res \ "name" must_== (JsString("krisp0"))
       res \ "password" must_== (JsString("123"))
+    }
+  }
+
+  /*------------- Campaign ---------------------------------------------------*/
+  "toJson - Campaign" should {
+    sequential
+
+    "take TRUE data" in {
+      val date = iso_fmt.parseDateTime("2013-01-01T12:00:00.000+04:00")
+
+      val data = Campaign(_login = "krisp0", _token = "123", start_date = date)
+
+      val res = toJson[Campaign](data)
+
+      res \ "_login" must_== (JsString("krisp0"))
+      res \ "_token" must_== (JsString("123"))
+      (res \ "start_date").as[DateTime] must_== (date)
+    }
+  }
+  /*------------- List[Campaign] ---------------------------------------------------*/
+  "toJson - List[Campaign]" should {
+    sequential
+
+    "take TRUE data" in {
+      val date = iso_fmt.parseDateTime("2013-01-01T12:00:00.000+04:00")
+
+      val data = List(
+        Campaign(_login = "krisp0", _token = "123", start_date = date),
+        Campaign(_login = "0psikr", _token = "321", start_date = date))
+
+      val res = toJson[List[Campaign]](data)
+
+      res \\ "_login" map (_.as[String]) must_== (List("krisp0", "0psikr"))
+      res \\ "_token" map (_.as[String]) must_== (List("123", "321"))
+      res \\ "start_date" map (_.as[DateTime]) must_== (List(date, date))
     }
   }
 }
