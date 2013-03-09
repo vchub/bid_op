902594d5de9141e8eb7597a9a7354cc77caa46cc
krispo <konstantin.skipor@gmail.com>
2013-03-09 10:45:12 +0400

    updated main page with clock

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,47 +2,43 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import views.html.index
 
 import play.api.data._
 import play.api.data.Forms._
 import domain._
 import dao.squerylorm._
 
-case class fullForm(
-  username: domain.User,
-  network: domain.Network,
-  campaign: domain.Campaign)
+import play.api.libs.{ Comet }
+import play.api.libs.iteratee._
 
 object Application extends Controller {
 
-  val fForm: Form[fullForm] = Form(
-
-    // Define a mapping that will handle fullForm values
-    mapping(
-      "username" -> nonEmptyText,
-      "network" -> nonEmptyText,
-      "campaign_id" -> optional(number(min = 0))) // The mapping signature matches the fullForm case class signature,
-      // so we can use default apply/unapply functions here
-      {
-        (username, network, campaign_id) =>
-          fullForm(
-            User.select(username).get,
-            Network.select(network).get,
-            Campaign.select(username, network).filter(_.id == campaign_id.get).head)
-      } {
-        fullForm =>
-          Some(fullForm.username.name,
-            fullForm.network.name,
-            Some(fullForm.campaign.id.toInt))
-      })
-
   /**
-   * Display an empty form.
+   * A String Enumerator producing a formatted Time message every 100 millis.
+   * A callback enumerator is pure an can be applied on several Iteratee.
    */
+  lazy val clock: Enumerator[String] = {
+
+    import play.api.libs.concurrent._
+    import scala.concurrent.duration._
+    import play.api.libs.concurrent.Execution.Implicits._
+
+    import java.util._
+    import java.text._
+
+    val dateFormat = new SimpleDateFormat("HH mm ss")
+
+    Enumerator.generateM {
+      Promise.timeout(Some(dateFormat.format(new Date)), 100 milliseconds)
+    }
+  }
 
   def index = Action {
-    Ok(views.html.index(fForm))
+    Ok(views.html.index())
+  }
+
+  def liveClock = Action {
+    Ok.stream(clock &> Comet(callback = "parent.clockChanged"))
   }
 
   def sample(id: String, c: String) = Action {
